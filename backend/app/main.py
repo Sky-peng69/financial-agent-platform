1cfe098544620f0352628edffe5f13d8cc7e0c36
truongthimy405-cell <truongthimy405@gmail.com>
2026-09-11 16:22:19 +0800

    auto: save work 2026-09-11_16:22

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
+        "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
         os.getenv("FRONTEND_URL", ""),
         # Netlify 部署域名
